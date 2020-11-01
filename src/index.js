06658bec5f41a4754781fd4a7f0c350915ae3410
Bara Maresova <bbmaresova@gmail.com>
2020-11-01 11:24:25 +0100

    nav bar fixed

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navbtn = document.getElementById('nav-btn');
+const nav = document.querySelector('nav');
+const navItems = document.querySelectorAll('.navItem');
+
+navbtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+navItems.forEach((nl) => {
+  nl.addEventListener('click', () => {
+    nav.classList.toggle('nav-closed');
+  });
+});
