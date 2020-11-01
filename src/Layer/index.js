e1bb1a8a9593b9cce6904bc7d09f37d12791b237
Bara Maresova <bbmaresova@gmail.com>
2020-11-01 15:16:45 +0100

    work on drinks, new sub-folders and components

--- src/Layer/index.js
+++ src/Layer/index.js
@@ -1,7 +1,23 @@
 import './style.css';
 
+
+const ingredients = [
+  { color: '#feeeca', label: 'mléčná pěna' },
+  { color: '#fed7b0', label: 'teplé mléko' },
+  { color: '#613916', label: 'espresso' },
+];
+
 export const Layer = (props) => {
   return `<div class="layer">
   <div class="layer__color" style="background-color: ${props.color}"></div>
               <div class="layer__label">${props.label}</div></div>`;
 };
+
+export const filledLayer = (props) => {
+  let result = '';
+  for (let i = 0; i < props.ingredients.length; i++) {
+    result += Layer(props.ingredients[i]);
+  }
+  return result;
+};
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './style.css';
-import {Layer} from "./Layer";
+import { Drink } from "./Drink"
 
 console.log('funguju!');
 
@@ -18,60 +18,20 @@ navItems.forEach((nl) => {
   });
 });
 
-const orderbtn = document.querySelector('.order-btn');
-const drinkCup = document.querySelector('.drink__cup');
-let ordered = false;
+const drinksList = document.querySelector('.drinks-list');
 
-const toggleButton = () => {
-  drinkCup.classList.toggle('drink__cup--selected');
-
-  if (ordered === false) {
-    orderbtn.textContent = 'Zrušit';
-    ordered = true;
-  } else {
-    orderbtn.textContent = 'Objednat';
-    ordered = false;
-  }
-};
-
-orderbtn.addEventListener('click', toggleButton);
-
-const drinkInfo = document.querySelector('.drink__info');
-const ingredients = [
-  { color: '#feeeca', label: 'mléčná pěna' },
-  { color: '#fed7b0', label: 'teplé mléko' },
-  { color: '#613916', label: 'espresso' },
-];
-
-const filledLayer = (props) => {
-  let result = '';
-  for (let i = 0; i < props.ingredients.length; i++) {
-    result += Layer(props.ingredients[i]);
-  }
-  return result;
-};
-
-drinkInfo.innerHTML += filledLayer({ingredients:ingredients});
-
-/* `<div
-                    class="layer__color"
-                    style="background-color: #feeeca"
-                  ></div>
-                  <div class="layer__label">mléčná pěna</div>
-                </div>
-                <div class="layer">
-                  <div
-                    class="layer__color"
-                    style="background-color: #fed7b0"
-                  ></div>
-                  <div class="layer__label">teplé mléko</div>
-                </div>
-
-                <div class="layer">
-                  <div
-                    class="layer__color"
-                    style="background-color: #613916"
-                  ></div>
-                  <div class="layer__label">espresso</div>
-                </div>
-              </div>` */
+drinksList.appendChild = Drink({
+  id: 'romano',
+  name: 'Romano',
+  ordered: false,
+  layers: [
+    {
+      color: '#fbdf5b',
+      label: 'citrón',
+    },
+    {
+      color: '#613916',
+      label: 'espresso',
+    },
+  ],
+});
