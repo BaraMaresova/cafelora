10d862cffe84357fe9ac19118a801859ebe5da44
Bara Maresova <bbmaresova@gmail.com>
2020-11-01 12:00:05 +0100

    toggle button done

--- src/index.js
+++ src/index.js
@@ -16,3 +16,21 @@ navItems.forEach((nl) => {
     nav.classList.toggle('nav-closed');
   });
 });
+
+const orderbtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+let ordered = false;
+
+const toggleButton = () => {
+  drinkCup.classList.toggle('drink__cup--selected');
+
+  if (ordered === false) {
+    orderbtn.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    orderbtn.textContent = 'Objednat';
+    ordered = false;
+  }
+};
+
+orderbtn.addEventListener('click', toggleButton);
